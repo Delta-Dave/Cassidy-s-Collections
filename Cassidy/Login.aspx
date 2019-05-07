687278cbc513ceccac89be89a9ec45bb6ef5a621
8361689 <8361689@student.cms.k12.nc.us>
2019-05-07 10:07:02 -0400

    Added login page

---
+++ Cassidy/Login.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Cassidy.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    &nbsp; User Name:&nbsp;
+        <asp:TextBox ID="UserNameTB" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="rfvUser" ErrorMessage="Please enter Username" ControlToValidate="UserNameTB" runat="server" />
+        <br />
+&nbsp;Password:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="PasswordTB" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" ErrorMessage="Please enter Username" ControlToValidate="PasswordTB" runat="server" />
+        <br />
+        <asp:Button ID="LoginBtn" runat="server" OnClick="LoginBtn_Click" Text="Login" />
+    
+    </div>
+        <asp:Label ID="UserID" runat="server" Text="User ID: "></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Cassidy/Login.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Cassidy
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoginBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from UserInformation where UserName =@username and Password=@password", con);
+            cmd.Parameters.AddWithValue("@username", UserNameTB.Text);
+            cmd.Parameters.AddWithValue("@password", PasswordTB.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                Response.Redirect("Details.aspx");
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<script language='javascript'>alert('Invalid Username and Password')</script>");
+            }
+        }
+    }
+}
