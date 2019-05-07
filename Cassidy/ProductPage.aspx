e8e31e8e63cb02eb60c97054785934e604939b27
8361689 <8361689@7508TD0104W1014>
2019-05-07 08:49:43 -0400

    Add project files.

---
+++ Cassidy/ProductPage.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProductPage.aspx.cs" Inherits="Cassidy.ProductPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="products">
+        <div class="product">
+            <asp:Image ID="Image1" runat="server" ImageUrl="2018-12-31.jpg" Width="200px" />
+            <br />
+            This is a Product Label</div>
+            
+    </div>
+    </form>
+</body>
+</html>
---
+++ Cassidy/ShowImage.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//https://www.dotnetcurry.com/ShowArticle.aspx?ID=129
+
+namespace Cassidy
+{
+    /// <summary>
+    /// Summary description for ShowImage
+    /// </summary>
+    public class ShowImage : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
