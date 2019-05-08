9a2851b704082ba053a979cd35ecea88f82c24e4
8361689 <8361689@student.cms.k12.nc.us>
2019-05-08 09:26:56 -0400

    old commits. Just getting synced

--- Cassidy/Login.aspx.designer.cs
+++ Cassidy/Login.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace Cassidy {
         protected global::System.Web.UI.WebControls.TextBox UserNameTB;
         
         /// <summary>
-        /// rfvUser control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvUser;
-        
-        /// <summary>
         /// PasswordTB control.
         /// </summary>
         /// <remarks>
@@ -47,15 +38,6 @@ namespace Cassidy {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox PasswordTB;
-        
-        /// <summary>
-        /// RequiredFieldValidator1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
         
         /// <summary>
         /// LoginBtn control.
