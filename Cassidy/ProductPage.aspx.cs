44984315f44367853d7eaa583e59437cbdf4d864
8361689 <8361689@student.cms.k12.nc.us>
2019-05-09 09:45:41 -0400

    Set up to work at school

--- Cassidy/ProductPage.aspx.cs
+++ Cassidy/ProductPage.aspx.cs
@@ -14,16 +14,16 @@ namespace Cassidy
         protected void Page_Load(object sender, EventArgs e)
         {
             int id = 1;
-            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["home"].ConnectionString);
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString);
             con.Open();
-            SqlCommand cmd = new SqlCommand("select ProductImage from Products where ProductID = @id", con);
+            SqlCommand cmd = new SqlCommand("select ImagePath from Products where ProductID = @id", con);
             cmd.Parameters.AddWithValue("@id", id);
             Image1.ImageUrl = "" + cmd.ExecuteScalar();
             if (Session["UserID"] != null)
             {
                 cmd = new SqlCommand("SELECT FirstName FROM Customers where UserID = @id", con);
                 cmd.Parameters.AddWithValue("@id", Session["UserID"]);
-                UserID.Text = "" + cmd.ExecuteScalar();
+                //UserID.Text = "" + cmd.ExecuteScalar();
             }
         }
 
