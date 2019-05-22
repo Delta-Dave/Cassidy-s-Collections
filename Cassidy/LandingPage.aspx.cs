4a2134c86d86a1b4a149dde00d0961664ab2fc32
12422969 <12422969@7508TD0104W1012>
2019-05-22 10:01:15 -0400

    edited logo position

--- Cassidy/LandingPage.aspx.cs
+++ Cassidy/LandingPage.aspx.cs
@@ -11,7 +11,7 @@ namespace Cassidy
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Session.Add("conString", "home");
+            Session.Add("conString", "sananda");
 
             if (Session["UserID"] != null)
             {
--- Cassidy/Profile.aspx.designer.cs
+++ Cassidy/Profile.aspx.designer.cs
@@ -83,5 +83,14 @@ namespace Cassidy {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button LogOut;
+        
+        /// <summary>
+        /// Cartbtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Cartbtn;
     }
 }
